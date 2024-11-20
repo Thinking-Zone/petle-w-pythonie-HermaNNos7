07e0a08305e3446cfbc16f987d26eee0d4722b02
HermaNNos7 <filip.hermann@thinkingzone.pl>
2024-11-20 22:08:29 +0100

    zadanie 6

---
+++ zadanie6.py
@@ -0,0 +1,2 @@
+# Pętla for w Pythonie powtarza wykonywanie bloku kodu z sekwencji do określonej liczby razy. 
+# Natomiast pętla while w Pythonie powtarza wykonywanie bloku kodu do momentu, aż warunek stanie się prawdziwy, co nie jest pewne. 
